e2fc2b1057ea3c5aa05b04a505212771c70b4978
Julian Grosshauser <julian@juliangrosshauser.com>
2015-09-12 00:28:15 +0200

    Create 5 variable bindings with 5 philosophers

--- src/main.rs
+++ src/main.rs
@@ -11,5 +11,9 @@ impl Philosopher {
 }
 
 fn main() {
-
+    let p1 = Philosopher::new("Judith Butler");
+    let p2 = Philosopher::new("Gilles Deleuze");
+    let p3 = Philosopher::new("Karl Marx");
+    let p4 = Philosopher::new("Emma Goldman");
+    let p5 = Philosopher::new("Michel Foucault");
 }
