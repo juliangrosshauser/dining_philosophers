65db00e0484a890e7a318750747761db39a0f8aa
Julian Grosshauser <julian@juliangrosshauser.com>
2015-09-12 00:25:24 +0200

    Add associated function `new()` to `Philosopher`

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@ struct Philosopher {
     name: String
 }
 
+impl Philosopher {
+    fn new(name: &str) -> Philosopher {
+        Philosopher {
+            name: name.to_string()
+        }
+    }
+}
+
 fn main() {
 
 }
